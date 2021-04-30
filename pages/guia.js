3d8bcd76b8952257e00917ad706832c4ae651236
Rychiv4 <rick_valladares@hotmail.com>
2021-04-29 18:52:59 -0600

    arreglando y agregando detalles para ver el pdf

---
+++ pages/guia.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import React from 'react'
+import { useRouter } from 'next/router'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+export default function Guia() {
+
+   const router = useRouter();
+
+   const [numPages, setNumPages] = useState(1);
+   const [page, setPage] = useState(1);
+
+   const onSuccess = (pdf)=>{
+      setNumPages(pdf._transport._numPages);
+   }
+
+   const nextPage = ()=>{
+      setPage(page + 1);
+   }
+
+   const prevPage = ()=>{
+      setPage(page - 1);
+   }
+
+   const returnToHome = ()=>{
+      router.push('/');
+   }
+
+   return (
+      <div>
+         <nav className="main-nav">
+            <img className="nav-logo" src="/media/chivas-con-el-covid.svg" alt="" data-aos="fade-left" onClick={returnToHome}/>
+            <a href="/guia.pdf" className="button pink" target="_blank" data-aos="fade-right" download>PDF</a>
+         </nav>
+         <div className="container">
+            <div className="w-100 d-flex justify-content-center flex-column align-items-center position-relative my-5">
+               <h3 className="mb-3">Guía para empleadores del sector del comercio</h3>
+               <Document file="/guia.pdf" width={1000} className="pdf">
+                  <Page pageNumber={page} onLoadSuccess={onSuccess}/>
+                  <div className="pdfarrows">
+                     <div className="mt-3 d-flex w-100 flex-direction-column justify-content-center align-items-center">
+                        <ul class="pagination pagination-lg">
+                           <li class="page-item">
+                              <a class="page-link" href="#!" onClick={prevPage}><FontAwesomeIcon icon={faChevronLeft}/></a>
+                           </li>
+                           <li class="page-item"><a class="page-link" href="#!">{page}</a></li>
+                           <li class="page-item">
+                              <a class="page-link" href="#!" onClick={nextPage}><FontAwesomeIcon icon={faChevronRight}/></a>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+               </Document>
+            </div>
+         </div>
+      </div>
+   )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Rellax from 'rellax'
 import AOS from "aos"
 
 import "aos/dist/aos.css";
 
 export default function Home() {
+
+   const router = useRouter();
 
    useEffect(()=>{
       
@@ -19,60 +22,60 @@ export default function Home() {
    }, []);
 
    var openPdf = ()=>{
-      window.open('/guia.pdf', '_blank').focus();
+      router.push('guia');
    }
 
-  return (
-    <div>
-      <Head>
-         <title>Chivas con el covid</title>
-         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="position-relative">
-         <nav className="main-nav">
-            <img className="nav-logo" src="/media/chivas-con-el-covid.svg" alt="" data-aos="fade-left"/>
-            <a href="/guia.pdf" className="button pink" target="_blank" data-aos="fade-right">PDF</a>
-         </nav>
-         <div className="lefttitle">
-            <span data-aos="fade-left"><span className="outline">NO. 001</span> CÁMARA DE COMERCIO E INDUSTRIA DE TEGUCIGALPA</span>
-         </div>
-         <div className="banner">
-            <img className="germen1 rellax" data-rellax-speed="-3" data-rellax-percentage="0.2" src="/media/germen.svg" alt=""/>
-            <img className="germen2 rellax" data-rellax-speed="-7" data-rellax-percentage="0.1" src="/media/germen.svg" alt=""/>
-            <img className="germen3 rellax" data-rellax-speed="-5" data-rellax-percentage="0.9" src="/media/germen.svg" alt=""/>
-            <div className="logo-wrapper" data-aos="fade-left">
-               <img className="germen4 rellax" data-rellax-speed="-5" data-rellax-percentage="1" src="/media/germen.svg" alt=""/>
-               <img className="germen" src="/media/germen-vivo.svg" alt=""/>
-               <img className="logo w-100" src="/media/chivas-con-el-covid-grande.svg" alt=""/>
-               <img className="germen-inferior" src="/media/germen-vivo.svg" alt=""/>
+   return (
+      <div>
+         <Head>
+            <title>Chivas con el covid</title>
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <div className="position-relative">
+            <nav className="main-nav">
+               <img className="nav-logo" src="/media/chivas-con-el-covid.svg" alt="" data-aos="fade-left"/>
+               <a href="/guia.pdf" className="button pink" target="_blank" data-aos="fade-right" download>PDF</a>
+            </nav>
+            <div className="lefttitle">
+               <span data-aos="fade-left"><span className="outline">NO. 001</span> CÁMARA DE COMERCIO E INDUSTRIA DE TEGUCIGALPA</span>
             </div>
-            <div className="container">
-               <div className="infoblocks">
-                  <div className="infoblock yellow" data-aos="fade-up" data-aos-delay="200" onClick={()=>{openPdf()}}>
-                     <span>TODOS PONGAMONOS</span>
-                     <span className="outline chivas">CHIVAS</span>
-                  </div>
-                  <div className="infoblock" data-aos="fade-up" data-aos-delay="400" onClick={()=>{openPdf()}}>
-                     <span>TODO EL MERCADO ESTÁ</span>
-                     <span className="outline chivas">CHIVAS</span>
-                  </div>
-                  <div className="infoblock pink" data-aos="fade-up" data-aos-delay="600" onClick={()=>{openPdf()}}>
-                     <span>PONÉ</span>
-                     <span className="outline chivas">CHIVAS</span>
-                     <span>A TUS COLABORADORES</span>
+            <div className="banner">
+               <img className="germen1 rellax" data-rellax-speed="-3" data-rellax-percentage="0.2" src="/media/germen.svg" alt=""/>
+               <img className="germen2 rellax" data-rellax-speed="-7" data-rellax-percentage="0.1" src="/media/germen.svg" alt=""/>
+               <img className="germen3 rellax" data-rellax-speed="-5" data-rellax-percentage="0.9" src="/media/germen.svg" alt=""/>
+               <div className="logo-wrapper" data-aos="fade-left">
+                  <img className="germen4 rellax" data-rellax-speed="-5" data-rellax-percentage="1" src="/media/germen.svg" alt=""/>
+                  <img className="germen" src="/media/germen-vivo.svg" alt=""/>
+                  <img className="logo w-100" src="/media/chivas-con-el-covid-grande.svg" alt=""/>
+                  <img className="germen-inferior" src="/media/germen-vivo.svg" alt=""/>
+               </div>
+               <div className="container">
+                  <div className="infoblocks">
+                     <div className="infoblock yellow" data-aos="fade-up" data-aos-delay="200" onClick={()=>{openPdf()}}>
+                        <span>TODOS PONGAMONOS</span>
+                        <span className="outline chivas">CHIVAS</span>
+                     </div>
+                     <div className="infoblock" data-aos="fade-up" data-aos-delay="400">
+                        <span>TODO EL MERCADO ESTÁ</span>
+                        <span className="outline chivas">CHIVAS</span>
+                     </div>
+                     <div className="infoblock pink" data-aos="fade-up" data-aos-delay="600">
+                        <span>PONÉ</span>
+                        <span className="outline chivas">CHIVAS</span>
+                        <span>A TUS COLABORADORES</span>
+                     </div>
                   </div>
                </div>
+               <a href="/guia.pdf" target="_blank" className="downloadbutton" data-aos="fade-up" download>
+                  <img src="/media/descargar.svg" alt=""/>
+               </a>
             </div>
-            <a href="/guia.pdf" target="_blank" className="downloadbutton" data-aos="fade-up">
-               <img src="/media/descargar.svg" alt=""/>
-            </a>
          </div>
+
+         <footer>
+            <img src="/media/footer-logos.png" alt="" data-aos="fade-up"/>
+         </footer>
+
       </div>
-
-      <footer>
-         <img src="/media/footer-logos.png" alt="" data-aos="fade-up"/>
-      </footer>
-
-    </div>
   )
 }
